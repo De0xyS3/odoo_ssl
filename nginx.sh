97cca9631890a1d1df59b356aefff1e95a38cca6
panconpalta <87393512+De0xyS3@users.noreply.github.com>
2023-08-30 17:46:20 -0500

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -2,89 +2,92 @@
 
 cd /etc/nginx/sites-available/
 
-echo "Ingresa el nombre de dominio"
-read input
-##### elif [[ "$input" == "" ]] || [[ "$input" == "" ]]; then
-echo "Ingresa el puerto utilizado del contendor"
+echo "Ingresa el dominio completo (ejemplo: midominio.com)"
+read full_domain
+
+# Obtener el nombre del dominio y su extensión.
+domain_name=$(echo $full_domain | cut -d. -f1)
+domain_extension=$(echo $full_domain | cut -d. -f2-)
+
+echo "Ingresa el puerto utilizado del contenedor"
 read puerto
 
-cat << EOF > $input.conf
+cat << EOF > $full_domain.conf
 
-# vim $input.conf 
+# vim $full_domain.conf 
 
-upstream odooserver-$input {
+upstream odooserver-$domain_name {
      server 127.0.0.1:$puerto;
- }
+}
 
- server {
-     listen [::]:80;
-     listen 80;
+server {
+    listen [::]:80;
+    listen 80;
 
-     server_name $input.com www.$input.com;
+    server_name $full_domain www.$full_domain;
 
-     return 301 https://$input.com/$request_uri;
- }
+    return 301 https://$full_domain/\$request_uri;
+}
 
- server {
-     listen [::]:443 ssl;
-     listen 443 ssl;
+server {
+    listen [::]:443 ssl;
+    listen 443 ssl;
 
-     server_name www.$input.com;
+    server_name www.$full_domain;
 
-     ssl_certificate /etc/letsencrypt/live/$input.com/fullchain.pem;
-     ssl_certificate_key /etc/letsencrypt/live/$input.com/privkey.pem;
+    ssl_certificate /etc/letsencrypt/live/$full_domain/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/$full_domain/privkey.pem;
 
-     return 301 https://$input.com\$request_uri;
- }
+    return 301 https://$full_domain\$request_uri;
+}
 
- server {
-     listen [::]:443 ssl http2;
-     listen 443 ssl http2;
+server {
+    listen [::]:443 ssl http2;
+    listen 443 ssl http2;
 
-     server_name $input.com;
+    server_name $full_domain;
 
-     ssl_certificate /etc/letsencrypt/live/$input.com/fullchain.pem;
-     ssl_certificate_key /etc/letsencrypt/live/$input.com/privkey.pem;
+    ssl_certificate /etc/letsencrypt/live/$full_domain/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/$full_domain/privkey.pem;
 
-     access_log /var/log/nginx/odoo_access.log;
-     error_log /var/log/nginx/odoo_error.log;
+    access_log /var/log/nginx/odoo_access.log;
+    error_log /var/log/nginx/odoo_error.log;
 
-     proxy_read_timeout 720s;
-     proxy_connect_timeout 720s;
-     proxy_send_timeout 720s;
-     proxy_set_header X-Forwarded-Host \$host;
-     proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-     proxy_set_header X-Forwarded-Proto \$scheme;
-     proxy_set_header X-Real-IP \$remote_addr;
+    proxy_read_timeout 720s;
+    proxy_connect_timeout 720s;
+    proxy_send_timeout 720s;
+    proxy_set_header X-Forwarded-Host \$host;
+    proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+    proxy_set_header X-Forwarded-Proto \$scheme;
+    proxy_set_header X-Real-IP \$remote_addr;
 
-     location / {
+    location / {
         proxy_redirect off;
-        proxy_pass http://odooserver-$input;
-     }
+        proxy_pass http://odooserver-$domain_name;
+    }
 
-     location ~* /web/static/ {
-         proxy_cache_valid 200 90m;
-         proxy_buffering on;
-         expires 864000;
-         proxy_pass http://odooserver-$input;
-     }
+    location ~* /web/static/ {
+        proxy_cache_valid 200 90m;
+        proxy_buffering on;
+        expires 864000;
+        proxy_pass http://odooserver-$domain_name;
+    }
 
-     gzip_types text/css text/less text/plain text/xml application/xml application/json application/javascript;
-     gzip on;
- }
+    gzip_types text/css text/less text/plain text/xml application/xml application/json application/javascript;
+    gzip on;
+}
 
 EOF
 
-ln -s /etc/nginx/sites-available/$input.conf /etc/nginx/sites-enabled/$input.conf
+ln -s /etc/nginx/sites-available/$full_domain.conf /etc/nginx/sites-enabled/$full_domain.conf
 nginx -t
 if [ $? -ne 0 ]; then
-        echo "Configuraciòn de nginx errònea"
+    echo "Configuración de nginx errónea"
 else
-        service nginx restart
+    service nginx restart
 
-echo "INSTALACION FINALIZADA. ODOO DOCKER CONTAINER SE ESTA EJECUTANDO" 
-echo "[`date +%m/%d/%Y-%H:%M`] INSTALACION FINALIZADA.. ODOO DOCKER CONTAINER ESTA INICIADO Y EJECUTANDO" 
-echo "Accede a odoo web desde el siguiente URL https://$input.com" 
-
+    echo "INSTALACION FINALIZADA. ODOO DOCKER CONTAINER SE ESTA EJECUTANDO"
+    echo "[`date +%m/%d/%Y-%H:%M`] INSTALACION FINALIZADA.. ODOO DOCKER CONTAINER ESTA INICIADO Y EJECUTANDO"
+    echo "Accede a odoo web desde el siguiente URL https://$full_domain"
 
 fi
